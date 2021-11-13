b3f7766b9da39785fea9f000d8aa7aa292127f4a
OlgaBerezinskaya <olgaberezinskaya@gmail.com>
2021-11-13 22:07:14 +0300

    pandas1

--- game.py
+++ game.py
@@ -12,9 +12,26 @@ melb_data['YearBuilt'] = melb_data['YearBuilt'].astype('int64')
 # print( melb_data.info())
 # print(melb_data['Type'].value_counts())
 # print(melb_data['Propertycount'].max())
-# 1075684.079455081
-# print(melb_data.describe(include=['float64']))
-pr = melb_data['Price'].mean()
-print(melb_data[(melb_data['Type']=='h')&(melb_data['Price']<3000000)]['Regionname'].value_counts())
+f = melb_data.copy()
+f = f.drop(['index', 'Coordinates'], axis=1)
+f['Date'] = pd.to_datetime(f['Date'])
+f['MonthSale'] = f['Date'].dt.month
+delta = f['Date'] - pd.to_datetime('2016-01-01') # – разница в днях между фикс датой и датой
 
+AgeBuilding = f['Date'].dt.year - f['YearBuilt']
+
+f['WeekdaySale'] = f['Date'].dt.dayofweek
+su = f[(f['WeekdaySale'] == 5) | (f['WeekdaySale'] == 6)]['Price'].sum()
+su1 = f[f['WeekdaySale'] == 5]['WeekdaySale'].sum()/5 +f[f['WeekdaySale'] == 6]['WeekdaySale'].sum()/6
+        
+        
+        
+
+serf = f['SellerG'].value_counts()[:49]
+
+f['SellerG'] = f['SellerG'].apply(lambda x: x if x in serf else 'other')
+
+ 
+
+print(f[f['SellerG'] == 'Nelson']['Price'].min() / f[f['SellerG'] == 'other']['Price'].min())
 
